a85936073a794a58199034b8a1e5d7e8d879bf8e
zivcroitoru <zuverc@gmail.com>
2025-06-21 01:14:33 +0300

    added collect with button

--- Assets/CollectbyTag.cs
+++ Assets/CollectbyTag.cs
@@ -1,24 +1,78 @@
 using UnityEngine;
 using TMPro;
+using System.Collections;
 using System.Collections.Generic;
 
 public class TriggerTest : MonoBehaviour
 {
-    public TMP_Text messageText;
+    public TMP_Text messageText;           // "Press E to collect..."
+    public TMP_Text collectedText;         // "Collected Bowl!"
+    public float interactRange = 0.005f;
+    public KeyCode interactKey = KeyCode.E;
+
     public static List<string> collectedItems = new List<string>();
     public static HashSet<string> usedItems = new HashSet<string>();
-void OnTriggerEnter(Collider other)
+
+    private GameObject currentTarget;
+
+ void Update()
 {
-    if (other.CompareTag("Collectible"))
+    currentTarget = null;
+    Collider[] hits = Physics.OverlapSphere(transform.position, interactRange);
+
+    float closestDot = -1f;
+
+    foreach (var hit in hits)
     {
-        string itemName = other.name.Replace("(Clone)", "").Trim();
-        collectedItems.Add(itemName);
+        if (!hit.CompareTag("Collectible"))
+            continue;
 
-        Debug.Log("Collected item: " + itemName);
-        Debug.Log("Current total: " + collectedItems.Count);
+        float dist = Vector3.Distance(hit.transform.position, transform.position);
+        if (dist > interactRange)
+            continue;
 
-        Destroy(other.gameObject);
+        Vector3 dirToHit = (hit.transform.position - transform.position).normalized;
+        float dot = Vector3.Dot(transform.forward, dirToHit);
+
+        if (dot > 0.01f && dot > closestDot)
+        {
+            currentTarget = hit.gameObject;
+            closestDot = dot;
+        }
+    }
+
+    if (currentTarget != null)
+    {
+        string itemName = currentTarget.name.Replace("(Clone)", "").Trim();
+        messageText.text = $"Press E to collect {itemName}";
+        messageText.gameObject.SetActive(true);
+
+        if (Input.GetKeyDown(interactKey))
+        {
+            Collect(currentTarget, itemName);
+        }
+    }
+    else
+    {
+        messageText.gameObject.SetActive(false);
     }
 }
 
+    void Collect(GameObject obj, string itemName)
+    {
+        collectedItems.Add(itemName);
+        Debug.Log("Collected item: " + itemName);
+        Debug.Log("Current total: " + collectedItems.Count);
+
+        StartCoroutine(ShowCollectedText(itemName));
+        Destroy(obj);
+    }
+
+    IEnumerator ShowCollectedText(string itemName)
+    {
+        collectedText.text = $"Collected {itemName}!";
+        collectedText.gameObject.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        collectedText.gameObject.SetActive(false);
+    }
 }
