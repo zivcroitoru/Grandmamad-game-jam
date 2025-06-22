4110ccbb0f82a81d167d78cec9d28173480f8e32
zivcroitoru <zuverc@gmail.com>
2025-06-22 14:06:45 +0300

    more tutorial fixes

--- Assets/TutorialTXT.cs
+++ Assets/TutorialTXT.cs
@@ -9,12 +9,10 @@ public class TutorialManager : MonoBehaviour
     [Header("UI Elements")]
     public GameObject tutorialPanel;
     public TMP_Text tutorialText;
-    public Button dismissButton;
 
     [Header("Mobile UI Elements")]
     public GameObject cameraJoystick;
     public GameObject moveJoystick;
-    public GameObject pickupButton;
 
     private bool tutorialDismissed = false;
 
@@ -27,27 +25,42 @@ public class TutorialManager : MonoBehaviour
         tutorialPanel.SetActive(true);
         AudioManager.Instance?.PauseMusic();
 
-        if (dismissButton != null)
-            dismissButton.onClick.AddListener(DismissTutorial);
-
         bool isMobile = Application.isMobilePlatform;
 
-tutorialText.text =
-    "<size=30><b><color=#FFFFFF>Help Granny reach the</color></b></size>\n" +
-    "<size=30><b><color=#FFFFFF>shelter in time</color></b></size>\n" +
-    "<size=24><color=#DDDDDD>Bring her comfort items along the way</color></size>\n" +
-    "<size=24><color=#DDDDDD>to keep her calm.</color></size>\n\n" +
-    "<align=center><size=38><b><color=#FFD700>How long\ncan you last?</color></b></size></align>\n\n" +
-    "<align=center><size=22><color=#EEEEEE>Move: <b>Left/Right Joystick</b></color></size></align>\n" +
-    "<align=center><size=22><color=#EEEEEE>Collect: <b>Tap Button</b></color></size></align>\n\n" +
-    "<align=center><size=20><color=#AAAAAA>Tap anywhere to begin</color></size></align>";
+        if (isMobile)
+        {
+            tutorialText.text =
+                "<size=30><b><color=#FFFFFF>Help Granny reach the</color></b></size>\n" +
+                "<size=30><b><color=#FFFFFF>shelter in time</color></b></size>\n" +
+                "<size=24><color=#DDDDDD>Bring her comfort items along the way</color></size>\n" +
+                "<size=24><color=#DDDDDD>to keep her calm.</color></size>\n\n" +
+                "<align=center><size=38><b><color=#FFD700>How long\ncan you last?</color></b></size></align>\n\n" +
+                "<align=center><size=22><color=#EEEEEE>Move: <b>Left/Right Joystick</b></color></size></align>\n" +
+                "<align=center><size=22><color=#EEEEEE>Collect: <b>Tap Button</b></color></size></align>\n\n" +
+                "<align=center><size=20><color=#AAAAAA>Tap anywhere to begin</color></size></align>";
+        }
+        else
+        {
+            tutorialText.text =
+                "<size=30><b><color=#FFFFFF>Help Granny reach the</color></b></size>\n" +
+                "<size=30><b><color=#FFFFFF>shelter in time</color></b></size>\n" +
+                "<size=24><color=#DDDDDD>Bring her comfort items along the way</color></size>\n" +
+                "<size=24><color=#DDDDDD>to keep her calm.</color></size>\n\n" +
+                "<align=center><size=38><b><color=#FFD700>How long\ncan you last?</color></b></size></align>\n\n" +
+                "<align=center><size=22><color=#EEEEEE>Move: <b>WASD and mouse</b></color></size></align>\n" +
+                "<align=center><size=22><color=#EEEEEE>Collect: <b>E key</b></color></size></align>\n\n" +
+                "<align=center><size=20><color=#AAAAAA>Click to begin</color></size></align>";
+        }
 
-        // Show joysticks immediately if mobile
         if (isMobile)
         {
             if (cameraJoystick != null) cameraJoystick.SetActive(true);
             if (moveJoystick != null) moveJoystick.SetActive(true);
-            if (pickupButton != null) pickupButton.SetActive(true);
+        }
+        else
+        {
+            if (cameraJoystick != null) cameraJoystick.SetActive(false);
+            if (moveJoystick != null) moveJoystick.SetActive(false);
         }
     }
 
